8a7f428a68cb86f249d66cb1ecd1e2113f33ec78
OlofTheBald <OlofTheBald@users.noreply.github.com>
2016-06-10 10:24:35 -0700

    Added tutorial 6 - SimpleMethod

---
+++ SimpleMethod/SimpleMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Teaches simple method implementation and why methods are important
+// Uses the Hello World concept for an ultra-simple example
+
+namespace SimpleMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld();
+            Console.ReadLine();
+        }
+
+        private static void HelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
